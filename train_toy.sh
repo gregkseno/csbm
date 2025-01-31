628b847b1dfa01698bc7412060f2eaaa9c044562
Ksenofontov Gregory <49430902+gregkseno@users.noreply.github.com>
2025-01-31 05:20:39 +0300

    Remove SLURM

--- train_toy.sh
+++ train_toy.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=toy_disc_sbm
-#SBATCH --partition=gpu
-#SBATCH --error=runs/test-%j.err
-#SBATCH --output=runs/test-%j.log
-#SBATCH --gpus=1
-#SBATCH --cpus-per-task=2
-#SBATCH --ntasks=1
-#SBATCH --nodes=1
-#SBATCH --time=1-00:00:00
-
-module load compilers/gcc-12.2.0
-module load gpu/cuda-12.3
-source activate disc_sbm
-
 accelerate launch \
     --num_processes=1 \
     --num_machines=1 \
@@ -23,4 +8,3 @@ accelerate launch \
         --config './configs/toy.yaml' \
         --exp_dir './experiments' \
         --data_dir './data'
-# --multi_gpu \     --cpu \
