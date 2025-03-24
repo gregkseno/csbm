bfcdd83e6069ac569e6b270c86882688065c62f1
gregkseno <g.ksenofontov@skoltech.ru>
2025-03-24 10:19:58 +0300

    Add AFHQ, fix discretization of toy data, enhance celeba initialization

--- src/csbm/data.py
+++ src/csbm/data.py
@@ -33,16 +33,22 @@ class BaseDataset(Dataset):
     def __len__(self):
         return len(self.dataset)
 
-    def continuous_to_discrete(self, batch: Union[torch.Tensor, np.ndarray], num_categories: int):
+    def continuous_to_discrete(
+        self, 
+        batch: Union[torch.Tensor, np.ndarray], 
+        num_categories: int,
+        quantize_range: Optional[Tuple[Union[int, float], Union[int, float]]] = None
+    ):
         if isinstance(batch, np.ndarray):
             batch = torch.tensor(batch)
-        batch_min = batch.min(dim=0).values
-        batch_max = batch.max(dim=0).values
-
-        discrete_batch = torch.zeros_like(batch, dtype=torch.int64)
-        for dim, (minn, maxx) in enumerate(zip(batch_min, batch_max)):  
-            bin_edges = torch.linspace(minn, maxx, num_categories - 1)
-            discrete_batch[:, dim] = torch.bucketize(batch[:, dim].contiguous(), bin_edges)
+        if quantize_range is None:
+            quantize_range = (-3, 3)
+        bin_edges = torch.linspace(
+            quantize_range[0], 
+            quantize_range[1], 
+            num_categories - 1
+        )
+        discrete_batch = torch.bucketize(batch, bin_edges)
         return discrete_batch
     
     def repeat(self, n: int, max_len: int):
@@ -151,38 +157,60 @@ class CelebaDataset(BaseDataset):
     
     def __init__(
         self, 
-        sex: Literal['male', 'female'], 
+        sex: Literal['male', 'female', 'both'], 
         data_dir: str,
         size: Optional[int] = None, 
         train: bool = True,
+        split: int | float = 162771, # from original dataset
+        use_quantized: bool = True,
         return_names: bool = False
     ):
         self.train = train
+        self.use_quantized = use_quantized
         self.size = size
         self.return_names = return_names
         self.data_dir= data_dir
 
-        attrs = pd.read_csv(os.path.join(data_dir, 'celeba', 'list_attr_celeba.csv'))
-        if sex == 'male':
-            attrs = attrs[attrs['Male'] != -1] # only males
-        else:
-            attrs = attrs[attrs['Male'] == -1]
-        split = pd.read_csv(os.path.join(data_dir, 'celeba', 'list_eval_partition.csv'))
+        subset = pd.read_csv(os.path.join(data_dir, 'celeba', 'list_attr_celeba.csv'))
 
-        if self.train:
-            split = split[split['partition'] == 0]
-            image_names = pd.merge(attrs, split, on=['image_id'], how='inner')
-            image_names['image_id'] = image_names['image_id'].str.removesuffix('.jpg') + '.npy'
-            sub_folder = 'quantized'
+        if isinstance(split, int): 
+            # this logic mathches setup of previously trained models
+            subset = subset.iloc[:split] if train else subset.iloc[split:]
+            if sex == 'male':
+                subset = subset[subset['Male'] != -1]
+            elif sex == 'female':
+                subset = subset[subset['Male'] == -1]
+            else:
+                subset = subset
         else:
-            split = split[split['partition'] != 0]
-            image_names = pd.merge(attrs, split, on=['image_id'], how='inner')
+            # this logic mathches asbm setup
+            male_subset = subset[subset['Male'] != -1]
+            female_subset = subset[subset['Male'] == -1]
+            male_split_index, female_split_index = int(len(male_subset) * split), int(len(female_subset) * split)
+            
+            male_subset = male_subset[:male_split_index] if train else male_subset.iloc[male_split_index:]
+            female_subset = female_subset[:female_split_index] if train else female_subset.iloc[female_split_index:]
+
+            if sex == 'male':
+                subset = male_subset
+            elif sex == 'female':
+                subset = female_subset
+            else:
+                subset = pd.concat([male_subset, female_subset], ignore_index=True)
+                subset = subset.sort_values(by='image_id').reset_index(drop=True)
+
+
+        if use_quantized:
+            sub_folder = 'quantized'
+            subset['image_id'] = subset['image_id'].str.removesuffix('.jpg') + '.npy'
+        else:
             sub_folder = 'raw'
-        self.image_names = image_names['image_id']
+
+        self.image_names = subset['image_id']
         self.dataset = [os.path.join(data_dir, 'celeba', 'img_align_celeba', sub_folder, image) for image in self.image_names.tolist()]
 
     def __getitem__(self, index):
-        if self.train:
+        if self.train and self.use_quantized:
             image = torch.from_numpy(np.load(self.dataset[index]))
         else:
             transform = transforms.Compose([
@@ -243,6 +271,95 @@ class CelebaDataset(BaseDataset):
                 file_name = image_path.split('/')[-1].split('.')[0]
                 image_path = os.path.join(save_path, file_name)
                 np.save(image_path, encoded_image)  
+
+
+class AFHQDataset(BaseDataset):
+    transform: Optional[transforms.Compose] = None
+    
+    def __init__(
+        self, 
+        animal_type: Literal['cat', 'wild', 'dog'], 
+        data_dir: str,
+        size: Optional[int] = None, 
+        train: bool = True,
+        use_quantized: bool = True,
+        return_names: bool = False
+    ):
+        self.train = train
+        self.use_quantized = use_quantized
+        self.size = size
+        self.return_names = return_names
+        self.data_dir = data_dir
+        self.animal_type = animal_type
+
+        if train:
+            path = os.path.join(data_dir, 'afhq', 'train', animal_type)
+        else:
+            path = os.path.join(data_dir, 'afhq', 'test', animal_type)
+
+        if use_quantized:
+            self.dataset = [os.path.join(path, image) for image in os.listdir(path) if image.endswith('.npy')]
+        else:
+            self.dataset = [os.path.join(path, image) for image in os.listdir(path) if image.endswith('.png')]
+
+    def __getitem__(self, index):
+        if self.use_quantized:
+            image = torch.from_numpy(np.load(self.dataset[index]))
+        else:
+            transform = transforms.Compose([
+                transforms.Resize((self.size, self.size)),
+                transforms.ToTensor(),
+            ])
+            image = Image.open(self.dataset[index])
+            image = image.convert('RGB')
+            image = transform(image)
+
+        if self.return_names:
+           return image, self.dataset[index].split('/')[-1]
+        return image
+
+    def __len__(self):
+        return len(self.dataset)
+    
+    def get_by_filename(self, index):
+        transform = transforms.Compose([
+                transforms.Resize((self.size, self.size)),
+                transforms.ToTensor(),
+        ])
+        image = Image.open(os.path.join(self.data_dir, 'afhq', self.animal_type, index))
+        image = image.convert('RGB')    
+        image = transform(image)
+        return image
+    
+    def repeat(self, n: int, max_len: int):
+        self.dataset = self.dataset * n
+        self.dataset = self.dataset[:max_len]
+
+    @staticmethod
+    def quantize_train(
+        model: nn.Module, 
+        data_dir: str,
+        size: int = 128, 
+        batch_size: int = 32,
+    ):
+        load_transform = transforms.Compose([
+            transforms.Resize((size, size)),
+            transforms.ToTensor(),
+        ])
+
+        data_dir = os.path.join(data_dir, 'afhq')
+        dataset = ImageFolder(data_dir, transform=load_transform, allow_empty=True) # allow_eppty because it will be handled in next line
+
+        dataloader = DataLoader(dataset, batch_size=batch_size)
+        for images, image_paths in tqdm(dataloader, file=sys.stdout):
+            images = images.to(model.device)
+            encoded_images = model.encode_to_cats(images).cpu().detach().numpy()
+            for encoded_image, image_path in zip(encoded_images, image_paths):
+                image_path = image_path.split('/')
+                file_path = image_path[:-1]
+                file_name = image_path[-1].split('.')[0]
+                image_path = os.path.join(*file_path, file_name)
+                np.save(image_path, encoded_image)
 
 
 class CouplingDataset(BaseDataset):
