c10ea112192b180ecc7b5638ef46ba762865dfef
gregkseno <g.ksenofontov@skoltech.ru>
2025-04-09 15:37:52 +0300

    Retrain tokenizer

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -4,7 +4,7 @@ import sys
 from omegaconf import OmegaConf
 
 from tokenizers import Tokenizer
-from tokenizers import models, pre_tokenizers, decoders, trainers
+from tokenizers import models, pre_tokenizers, decoders, trainers, processors
 
 sys.path.append('src')
 from csbm.data import YelpDataset
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     args = OmegaConf.load(args.config)
 
     # Load the dataset for training the tokenizer
-    dataset = YelpDataset(sentiment='all', data_dir=data_dir, split=1.0)
+    dataset = YelpDataset(sentiment='all', data_dir=data_dir, split='all')
     print(f"Loaded dataset with {len(dataset)} samples.")
     print(dataset[0])
 
@@ -29,6 +29,12 @@ if __name__ == '__main__':
     tokenizer = Tokenizer(models.Unigram()) # type: ignore
     tokenizer.pre_tokenizer = pre_tokenizers.Metaspace() # type: ignore
     tokenizer.decoder = decoders.Metaspace() # type: ignore
+    tokenizer.post_processor = processors.TemplateProcessing(
+        single="<s> $A",
+        special_tokens=[
+            ("<s>", 2),
+        ],
+    ) # type: ignore
 
     trainer = trainers.UnigramTrainer(
         vocab_size=args.data.num_categories, 
@@ -36,10 +42,10 @@ if __name__ == '__main__':
         unk_token="<unk>",
         show_progress=True,
     )
-    # print("Training tokenizer...")
-    # tokenizer.train_from_iterator(
-    #     (str(x) for x in dataset),
-    #     trainer=trainer,
-    # )
+    print("Training tokenizer...")
+    tokenizer.train_from_iterator(
+        (str(x) for x in dataset),
+        trainer=trainer,
+    )
     
-    # tokenizer.save("checkpoints/tokenizer-yelp.json")
+    tokenizer.save(args.tokenizer.path)
--- train_tokenizer.sh
+++ train_tokenizer.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=train-tokenizer
-#SBATCH --partition=ais-gpu
-#SBATCH --reservation=HPC-2507
+#SBATCH --partition=ais-cpu
 #SBATCH --error=runs/train-tokenizer-%j.err
 #SBATCH --output=runs/train-tokenizer-%j.log
-#SBATCH --gpus=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=50G
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
